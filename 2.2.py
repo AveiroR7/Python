ae327a7daf1ee33f4a9701e5cb7d9c7476504f60
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2021-02-13 18:52:44 +0530

    Create 2.2.py

---
+++ 2.2.py
@@ -0,0 +1,5 @@
+#Write a Python program to find the remainder when a number is divided by Z(Integer)
+a= int(input("Enter first number:")) #5
+b= int(input("Enter second number:")) #2
+
+print("The remainder of the numbers is ",a%b) #1
