3ff5b8084204f646169dc211534731c3b34b99e4
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2021-02-13 20:10:27 +0530

    Create 2.5.py

---
+++ 2.5.py
@@ -0,0 +1,4 @@
+#Write a Python program to find the average of two numbers entered by the user.
+a= int(input("Enter the first number:"))
+b= int(input("Enter the second number:"))
+print("The average of two numbers is",((a+b)/2))
